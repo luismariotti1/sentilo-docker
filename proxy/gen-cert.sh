b4c94044f798f65f6fc54ff58be0f61c57148f96
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2022-10-19 12:04:49 -0300

    update

--- proxy/gen-cert.sh
+++ proxy/gen-cert.sh
@@ -6,14 +6,14 @@ rm certs/server/*.pem
 rm certs/ca/*.srl
 
 # Generate a self-signed certificate for the proxy
-openssl req -nodes -x509 -newkey rsa:4096 -days 365 -keyout certs/ca/ca-key.pem -out certs/ca/ca-cert.pem -subj "/C=BR/ST=Parana/L=Toledo/O=Envcity/OU=IoT/CN=*.envcity.com/emailAddress=envcity@gmail.com"
+openssl req -nodes -x509 -newkey rsa:4096 -sha256 -days 365 -keyout certs/ca/ca-key.pem -out certs/ca/ca-cert.pem -subj "/C=BR/ST=Parana/L=Toledo/O=Envcity/OU=IoT/CN=*.envcity.com/emailAddress=envcity@gmail.com"
 
 # debug info
 #echo "CA's self-signed certificate"
 #openssl x509 -in certs/ca/ca-cert.pem -noout -text
 
 # Generate a certificate signing request for the proxy
-openssl req -nodes -newkey rsa:4096 -keyout certs/server/server-key.pem -out certs/server/server-req.pem -subj "/C=BR/ST=Parana/L=Toledo/O=Sentilo/OU=Server/CN=localhost.com/emailAddress=sentilo@gmail.com"
+openssl req -nodes -newkey rsa:4096 -sha256 -keyout certs/server/server-key.pem -out certs/server/server-req.pem -subj "/C=BR/ST=Parana/L=Toledo/O=Sentilo/OU=Server/CN=localhost.com/emailAddress=sentilo@gmail.com"
 
 # Sign the proxy's certificate with the CA's certificate
-openssl x509 -req -in certs/server/server-req.pem -CA certs/ca/ca-cert.pem -CAkey certs/ca/ca-key.pem -CAcreateserial -out certs/server/server-cert.pem
+openssl x509 -req -sha256 -in certs/server/server-req.pem -CA certs/ca/ca-cert.pem -CAkey certs/ca/ca-key.pem -CAcreateserial -out certs/server/server-cert.pem
