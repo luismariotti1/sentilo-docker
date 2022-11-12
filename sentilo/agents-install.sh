1c36873b492d0e60567a626810384af8da05a47e
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2022-11-12 19:17:35 -0300

    update

--- sentilo/agents-install.sh
+++ sentilo/agents-install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # save agent path in variable
-sentilo_path=/sentilo
 agent_path=/sentilo/sentilo-agent-activity-monitor
 agent_server_path=$agent_path/target/appassembler/bin
+
+echo installing elasticsearch ...
 
 # check if appassembler folder exists
 if [ ! -d $agent_path"/target/appassembler/" ]; then
@@ -18,5 +19,21 @@ if [ ! -x sentilo-agent-activity-monitor-server ]; then
     chmod +x sentilo-agent-activity-monitor-server
 fi
 
-# execute the agent
-./sentilo-agent-activity-monitor-server
+agent_path=/sentilo/sentilo-agent-historian
+agent_server_path=$agent_path/target/appassembler/bin
+
+echo installing opentsdb ...
+
+# check if appassembler folder exists
+if [ ! -d $agent_path"/target/appassembler/" ]; then
+    cd $agent_path
+    mvn clean install; mvn package appassembler:assemble -P dev
+fi
+
+cd $agent_server_path
+
+# # check if the sentilo-agent-activity-monitor-server fille has +x permission
+if [ ! -x sentilo-agent-historian ]; then
+    chmod +x sentilo-agent-historian-server
+fi
+
